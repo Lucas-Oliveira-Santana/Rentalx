f85b726d5500d69fa085d5fb99ba7865ae970be4
LucasOliveiraSantana <lucasoliveirasantana.dev@gmail.com>
2022-10-31 08:14:32 -0300

    Refresh_token

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -6,7 +6,7 @@ import {CreateUserUseCase} from './CreateUserUseCase';
 class CreateUserController {
     async handle(req: Request, res: Response): Promise<Response> {
         const {name, email, password, driver_license} = req.body;
-
+        
         const createUserUseCase = container.resolve(CreateUserUseCase);
 
         await createUserUseCase.execute({
